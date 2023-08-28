48d819867601e9fa9f6f03c75a2f8bd55b40ec07
Oğulcan Gök <78494015+ogulcan-gok@users.noreply.github.com>
2023-08-28 10:51:08 +0300

    Update parser.py

--- flask_saml2/idp/parser.py
+++ flask_saml2/idp/parser.py
@@ -34,7 +34,8 @@ class AuthnRequestParser(XmlParser):
     @cached_property
     def acs_url(self) -> str:
         """The AssertionConsumerServiceURL attribute."""
-        return "https://srm.abdiibrahim.com.tr:8443/sap/saml2/sp/acs/100"
+        print(self._xpath_xml_tree('/samlp:AuthnRequest/@AssertionConsumerServiceURL'))
+        return self._xpath_xml_tree('/samlp:AuthnRequest/@AssertionConsumerServiceURL')[0]
     @cached_property
     def provider_name(self) -> str:
         """The ProviderName attribute, if it exists."""
